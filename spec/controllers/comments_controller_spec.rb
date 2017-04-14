87ddabec643a1567f49e56064589a955354148db
Alyson Stiffel <alysonstiffel@gmail.com>
2017-04-14 17:07:25 -0400

    comments

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -4,11 +4,9 @@ RSpec.describe CommentsController, type: :controller do
   describe "comments#create action" do
     it "should allow users to create comments on grams" do
       gram = FactoryGirl.create(:gram)
-
       user = FactoryGirl.create(:user)
       sign_in user
-
-      post :create, params: {gram_id: gram_id, comment: {message: 'awesome gram'}}
+      post :create, params: { gram_id: gram.id, comment: { message: 'awesome gram' } }
       expect(response).to redirect_to root_path
       expect(gram.comments.length).to eq 1
       expect(gram.comments.first.message).to eq "awesome gram"
@@ -16,7 +14,7 @@ RSpec.describe CommentsController, type: :controller do
 
     it "should require a user to be logged in to comment on a gram" do
       gram = FactoryGirl.create(:gram)
-      post :create, params: {gram_id: gram.id, comment: {message: 'awesome gram'}}
+      post :create, params: { gram_id: gram.id, comment: { message: 'awesome gram' } }
       expect(response).to redirect_to new_user_session_path
     end
 
